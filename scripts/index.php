31505419333d99b53024c534118d3010e1099f7e
= <=>
2018-04-15 16:22:24 +0200

    update to version 1.4.4 (1 new script)

--- scripts/index.php
+++ scripts/index.php
@@ -278,6 +278,16 @@
                     <li class="link_tips"><imp><a class="white" href="solution/QuickDraw_classification.ipynb" download="QuickDraw_classification.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
                 </ul>
             </div>
+            <div class="col-lg-8 col-lg-offset-2"> 
+                <p><t><?= _PUZZLE22_TITLE ?></t></p>
+                <p><more><?= _PUZZLE22_DESC ?></more></p>
+            </div>
+            <div class="col-lg-2 pro">
+                <ul>
+                    <li class="link_tips"><imp><a class="white" href="solution/Neuro_evolution.html" target="_blank"><?= _SHOW_SOLUTION ?></a></imp></li>
+                    <li class="link_tips"><imp><a class="white" href="solution/Neuro_evolution.ipynb" download="Neuro_evolution.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
+                </ul>
+            </div>
 		</div>
         </div>
     <section id="down" name="down"></section>
