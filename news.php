0121320d238a8314bdcba9832538ee93284f6c25
= <=>
2018-04-28 14:07:16 +0200

    add 2 script for v1.4.5

--- news.php
+++ news.php
@@ -43,6 +43,16 @@
             </div>
             
             <div class="col-lg-6">
+                <p><t><?= _NEWS24_TITLE ?></t><br/>
+                    <?= _NEWS24_DESC ?>
+                </p>
+            </div>
+            
+            <div class="col-lg-3">
+                <p><sm><?= _NEWS24_DATE ?></sm></p>
+            </div>
+            
+            <div class="col-lg-6 col-lg-offset-3">
                 <p><t><?= _NEWS23_TITLE ?></t><br/>
                     <?= _NEWS23_DESC ?>
                 </p>
--- scripts/index.php
+++ scripts/index.php
@@ -288,6 +288,26 @@
                     <li class="link_tips"><imp><a class="white" href="solution/Neuro_evolution.ipynb" download="Neuro_evolution.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
                 </ul>
             </div>
+            <div class="col-lg-8 col-lg-offset-2"> 
+                <p><t><?= _PUZZLE23_TITLE ?></t></p>
+                <p><more><?= _PUZZLE23_DESC ?></more></p>
+            </div>
+            <div class="col-lg-2 pro">
+                <ul>
+                    <li class="link_tips"><imp><a class="white" href="solution/exploration.html" target="_blank"><?= _SHOW_SOLUTION ?></a></imp></li>
+                    <li class="link_tips"><imp><a class="white" href="solution/exploration.Rmd" download="exploration.Rmd"><?= _DL_SOLUTION ?></a></imp></li>
+                </ul>
+            </div>
+            <div class="col-lg-8 col-lg-offset-2"> 
+                <p><t><?= _PUZZLE24_TITLE ?></t></p>
+                <p><more><?= _PUZZLE24_DESC ?></more></p>
+            </div>
+            <div class="col-lg-2 pro">
+                <ul>
+                    <li class="link_tips"><imp><a class="white" href="https://github.com/Coni63/DeepDream" target="_blank"><?= _SHOW_REPO ?></a></imp></li>
+                    <li class="link_tips"><imp><a class="white" href="https://www.youtube.com/watch?v=KW3CJ7i6LP4"  target="_blank"><?= _SHOW_VID ?></a></imp></li>
+                </ul>
+            </div>
 		</div>
         </div>
     <section id="down" name="down"></section>
