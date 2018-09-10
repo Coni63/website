4ecf58a7bf8433307e2abe68895f01df785a7500
= <=>
2018-09-10 18:03:03 +0200

    add a script (v1.4.21)

--- news.php
+++ news.php
@@ -43,6 +43,16 @@
             </div>
             
             <div class="col-lg-6">
+                <p><t><?= _NEWS40_TITLE ?></t><br/>
+                    <?= _NEWS40_DESC ?>
+                </p>
+            </div>
+            
+            <div class="col-lg-3">
+                <p><sm><?= _NEWS40_DATE ?></sm></p>
+            </div>
+            
+            <div class="col-lg-6 col-lg-offset-3">
                 <p><t><?= _NEWS39_TITLE ?></t><br/>
                     <?= _NEWS39_DESC ?>
                 </p>
