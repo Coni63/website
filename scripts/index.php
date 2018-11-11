5cbf3936e31949b97ca3883e42e670c0440c4169
= <=>
2018-11-11 13:22:28 +0100

    add a notebook (v1.4.25)

--- scripts/index.php
+++ scripts/index.php
@@ -560,6 +560,18 @@
                 </ul>
             </div>
             
+            <div class="col-lg-8 col-lg-offset-2"> 
+                <p><t><?= _PUZZLE46_TITLE ?></t></p>
+                <p><more><?= _PUZZLE46_DESC ?></more></p>
+            </div>
+
+            <div class="col-lg-2 pro">
+                <ul>
+                    <li class="link_tips"><imp><a class="white" href="solution/Policy+Search.html" target="_blank"><?= _SHOW_SOLUTION ?></a></imp></li>
+                    <li class="link_tips"><imp><a class="white" href="solution/Policy%20Search.ipynb" download="Policy%20Search.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
+                </ul>
+            </div>
+            
 		</div>
         </div>
     <section id="down" name="down"></section>
