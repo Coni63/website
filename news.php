d6c058db9327a87c90e8b29348467838432a20c5
= <=>
2018-11-18 11:09:37 +0100

    add a script (v1.4.26)

--- news.php
+++ news.php
@@ -43,6 +43,16 @@
             </div>
             
             <div class="col-lg-6">
+                <p><t><?= _NEWS45_TITLE ?></t><br/>
+                    <?= _NEWS45_DESC ?>
+                </p>
+            </div>
+            
+            <div class="col-lg-3">
+                <p><sm><?= _NEWS45_DATE ?></sm></p>
+            </div>
+            
+            <div class="col-lg-6 col-lg-offset-3">
                 <p><t><?= _NEWS44_TITLE ?></t><br/>
                     <?= _NEWS44_DESC ?>
                 </p>
--- scripts/index.php
+++ scripts/index.php
@@ -572,6 +572,18 @@
                 </ul>
             </div>
             
+            <div class="col-lg-8 col-lg-offset-2"> 
+                <p><t><?= _PUZZLE47_TITLE ?></t></p>
+                <p><more><?= _PUZZLE47_DESC ?></more></p>
+            </div>
+
+            <div class="col-lg-2 pro">
+                <ul>
+                    <li class="link_tips"><imp><a class="white" href="solution/PPO.html" target="_blank"><?= _SHOW_SOLUTION ?></a></imp></li>
+                    <li class="link_tips"><imp><a class="white" href="solution/PPO.ipynb" download="PPO.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
+                </ul>
+            </div>
+            
 		</div>
         </div>
     <section id="down" name="down"></section>
