770cdc3e1f8bd891d6c0b1ea85256334e6186c48
= <=>
2018-12-09 11:33:31 +0100

    add a script (v1.4.29)

--- news.php
+++ news.php
@@ -43,6 +43,16 @@
             </div>
             
             <div class="col-lg-6">
+                <p><t><?= _NEWS48_TITLE ?></t><br/>
+                    <?= _NEWS48_DESC ?>
+                </p>
+            </div>
+            
+            <div class="col-lg-3">
+                <p><sm><?= _NEWS48_DATE ?></sm></p>
+            </div>
+            
+            <div class="col-lg-6 col-lg-offset-3">
                 <p><t><?= _NEWS47_TITLE ?></t><br/>
                     <?= _NEWS47_DESC ?>
                 </p>
--- scripts/index.php
+++ scripts/index.php
@@ -608,6 +608,18 @@
                 </ul>
             </div>
             
+            <div class="col-lg-8 col-lg-offset-2"> 
+                <p><t><?= _PUZZLE50_TITLE ?></t></p>
+                <p><more><?= _PUZZLE50_DESC ?></more></p>
+            </div>
+
+            <div class="col-lg-2 pro">
+                <ul>
+                    <li class="link_tips"><imp><a class="white" href="solution/ETF_project/Benchmark.html" target="_blank"><?= _SHOW_SOLUTION ?></a></imp></li>
+                    <li class="link_tips"><imp><a class="white" href="solution/ETF_project/Benchmark.ipynb" download="Benchmark.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
+                </ul>
+            </div>
+            
 		</div>
         </div>
     <section id="down" name="down"></section>
