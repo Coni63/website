f52fc097a9ddd27bba017208ad60fe75661f0a20
= <=>
2019-01-20 11:54:04 +0100

    add a script (v1.5.1)

--- news.php
+++ news.php
@@ -43,6 +43,16 @@
             </div>
             
             <div class="col-lg-6">
+                <p><t><?= _NEWS52_TITLE ?></t><br/>
+                    <?= _NEWS52_DESC ?>
+                </p>
+            </div>
+            
+            <div class="col-lg-3">
+                <p><sm><?= _NEWS52_DATE ?></sm></p>
+            </div>
+            
+            <div class="col-lg-6 col-lg-offset-3">
                 <p><t><?= _NEWS51_TITLE ?></t><br/>
                     <?= _NEWS51_DESC ?>
                 </p>
--- scripts/index.php
+++ scripts/index.php
@@ -658,6 +658,18 @@
                 </ul>
             </div>
             
+            <div class="col-lg-8 col-lg-offset-2"> 
+                <p><t><?= _PUZZLE54_TITLE ?></t></p>
+                <p><more><?= _PUZZLE54_DESC ?></more></p>
+            </div>
+
+            <div class="col-lg-2 pro">
+                <ul>
+                    <li class="link_tips"><imp><a class="white" href="solution/Bayesian_Net.html" target="_blank"><?= _SHOW_SOLUTION ?></a></imp></li>
+                    <li class="link_tips"><imp><a class="white" href="solution/Bayesian_Net.ipynb" download="Bayesian_Net.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
+                </ul>
+            </div>
+            
 		</div>
         </div>
     <section id="down" name="down"></section>
