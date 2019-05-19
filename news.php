68cbb4d28d6de70956618f984d824c4989c54d0b
= <=>
2019-05-19 10:54:27 +0200

    Add a script (v1.5.10)

--- news.php
+++ news.php
@@ -43,6 +43,16 @@
             </div>
             
             <div class="col-lg-6">
+                <p><t><?= _NEWS61_TITLE ?></t><br/>
+                    <?= _NEWS61_DESC ?>
+                </p>
+            </div>
+            
+            <div class="col-lg-3">
+                <p><sm><?= _NEWS61_DATE ?></sm></p>
+            </div>
+            
+            <div class="col-lg-6 col-lg-offset-3">
                 <p><t><?= _NEWS60_TITLE ?></t><br/>
                     <?= _NEWS60_DESC ?>
                 </p>
--- scripts/index.php
+++ scripts/index.php
@@ -769,6 +769,18 @@
                 </ul>
             </div>
             
+            <div class="col-lg-8 col-lg-offset-2"> 
+                <p><t><?= _PUZZLE63_TITLE ?></t></p>
+                <p><more><?= _PUZZLE63_DESC ?></more></p>
+            </div>
+
+            <div class="col-lg-2 pro">
+                <ul>
+                    <li class="link_tips"><imp><a class="white" href="solution/cryptography/DiffieHellman.html" target="_blank"><?= _SHOW_SOLUTION ?></a></imp></li>
+                    <li class="link_tips"><imp><a class="white" href="solution/cryptography/DiffieHellman.ipynb" download="DiffieHellman.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
+                </ul>
+            </div>
+            
 		</div>
         </div>
     <section id="down" name="down"></section>
