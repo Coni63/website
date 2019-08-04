4950aa1d727105a7b6887db278157aeab397b8e4
= <=>
2019-08-04 14:23:31 +0200

    add a scipt (v1.5.15)

--- scripts/index.php
+++ scripts/index.php
@@ -828,6 +828,18 @@
                     <li class="link_tips"><imp><a class="white" href="solution/CAM/CAM.ipynb" download="CAM.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
                 </ul>
             </div>
+
+            <div class="col-lg-8 col-lg-offset-2"> 
+                <p><t><?= _PUZZLE68_TITLE ?></t></p>
+                <p><more><?= _PUZZLE68_DESC ?></more></p>
+            </div>
+
+            <div class="col-lg-2 pro">
+                <ul>
+                    <li class="link_tips"><imp><a class="white" href="solution/CAM/SeamCarvingClassification.html" target="_blank"><?= _SHOW_SOLUTION ?></a></imp></li>
+                    <li class="link_tips"><imp><a class="white" href="solution/CAM/SeamCarvingClassification.ipynb" download="SeamCarvingClassification.ipynb"><?= _DL_SOLUTION ?></a></imp></li>
+                </ul>
+            </div>
             
 		</div>
         </div>
